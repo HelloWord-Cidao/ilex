fe5bb680c9df71877e68e3b99516d80526f3a307
Peach Wang <peachwang@outlook.com>
2015-10-12 05:15:34 +0800

    ignore vendor/ & update Bootstrap.php

--- Test/Bootstrap.php
+++ Test/Bootstrap.php
@@ -3,3 +3,14 @@
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 \Ilex\Test::boot(__DIR__ . '/../Test/app', __DIR__ . '/../Test/runtime');
+
+include(__DIR__ . '/RouteTest.php');
+
+$RouteTest = new RouteTest();
+$RouteTest->testHelloWorld();
+$RouteTest->testPost();
+$RouteTest->testCallingController();
+$RouteTest->testControllerIndex();
+$RouteTest->testControllerFunction();
+$RouteTest->testControllerResolve();
+$RouteTest->testGroup();
