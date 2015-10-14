6672b2d3b5ee2945e5bba277186745544f9b267c
Peach Wang <peachwang@outlook.com>
2015-10-15 03:13:51 +0800

    all test cases pass!

--- Test/app/Controller/PlayController.php
+++ Test/app/Controller/PlayController.php
@@ -2,6 +2,8 @@
 
 namespace app\Controller;
 
+use \Ilex\Lib\Kit;
+
 /**
  * Class PlayController
  * @package app\Controller
@@ -16,20 +18,28 @@ class PlayController extends \Ilex\Base\Controller\Base
      */
     public function resolve($Router)
     {
+        Kit::log([__METHOD__, 'get / func'], FALSE);
         $Router->get('/', function () {
             return ('Come and play!');
         });
+        
+        Kit::log([__METHOD__, 'get /(num) $this, view'], FALSE);
         $Router->get('/(num)', $this, 'view');
 
         // Just a test for `group` inside a controller's `resolve`...
+        Kit::log([__METHOD__, 'group /play func'], FALSE);
         $Router->group('/play', function ($Router) {
             /** @var \Ilex\Core\Router $Router */
+            Kit::log([__METHOD__, 'get /(num) $this view'], FALSE);
             $Router->get('/(num)', $this, 'view');
             $Router->back();
+            return $Router->result();
         });
 
+        Kit::log([__METHOD__, 'group /no-back func'], FALSE);
         $Router->group('/no-back', function ($Router) {
             /** @var \Ilex\Core\Router $Router */
+            Kit::log([__METHOD__, 'get / func'], FALSE);
             $Router->get('/', function () {
                 return ('No back here...');
             });
@@ -38,11 +48,16 @@ class PlayController extends \Ilex\Base\Controller\Base
              * Add `$Router->get('(all)', ...)` or `$Router->get('.*')` to response.
              * Add `$Router->back()` to fallback.
              */
+            return $Router->result();
         });
 
+        Kit::log([__METHOD__, 'get (all) func'], FALSE);
         $Router->get('(all)', function ($url) {
             return ('Sorry but "' . substr($url, 1) . '" is not here. 404.');
         });
+
+        Kit::log([__METHOD__, 'return $Router->result()'], FALSE);
+        return $Router->result();
     }
 
     /**
