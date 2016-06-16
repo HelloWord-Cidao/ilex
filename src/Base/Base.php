c67ed004de9c9be7f1589b86b50cf4a54b00b04e
Peach Wang <peachwang@outlook.com>
2016-06-16 15:29:19 +0800

    update

--- src/Base/Base.php
+++ src/Base/Base.php
@@ -9,20 +9,22 @@ use \Ilex\Core\Loader;
  * Base class of controllers and models.
  * @package Ilex\Base
  * 
- * @method final protected static object loadModel(string $path, array $arg_list = [])
+ * @method final protected static object loadModel(string $path, array $arg_list = []
+ *                                           , boolean $with_instantiate = TRUE)
  */
 abstract class Base
 {
     /**
-     * @param string $path
-     * @param array  $arg_list 
+     * @param string  $path
+     * @param array   $arg_list 
+     * @param boolean $with_instantiate 
      * @return object
      */
-    final protected function loadModel($path, $arg_list = [])
+    final protected function loadModel($path, $arg_list = [], $with_instantiate = TRUE)
     {
         $handler_name = Loader::getHandlerFromPath($path);
         if (TRUE === is_null($this->$handler_name))
-            return ($this->$handler_name = Loader::model($path, $arg_list));
+            return ($this->$handler_name = Loader::model($path, $arg_list, $with_instantiate));
         else return $this->$handler_name;
     }
 }
